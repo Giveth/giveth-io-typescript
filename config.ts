838b7a79f67e1e5886776247f2e26b1079dc967c
Ramin <r.ramazanpour@aut.ac.ir>
2021-11-07 14:00:15 +0330

    First commit: configure packages, app env variables, next and typescript configs and IDE config

---
+++ config.ts
@@ -0,0 +1,5 @@
+const config = {
+  QRAPHQL_SERVER: 'https://dev.serve.giveth.io/graphql'
+}
+
+export default config
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      'gateway.pinata.cloud',
+      'imgur.com',
+      'i.imgur.com',
+      'feathers.beta.giveth.io',
+      'feathers.giveth.io',
+      'feathers.develop.giveth.io',
+      'ipfs.giveth.io'
+    ]
+  },
 }
