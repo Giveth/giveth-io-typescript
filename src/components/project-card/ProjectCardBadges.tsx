b0f511ad5520c1d59a1dbc22466d009087ea7580
Ramin <r.ramazanpour@aut.ac.ir>
2021-11-12 01:43:41 +0330

    implement project card hover state

--- src/components/project-card/ProjectCardBadges.tsx
+++ src/components/project-card/ProjectCardBadges.tsx
@@ -3,9 +3,10 @@ import Image from "next/image";
 import {Primary_Deep_800_Trans} from "../styled-components/Colors";
 import {FlexCenter} from "../styled-components/Grid";
 import {Subline_Bold} from "../styled-components/Typography";
-import VerificationBadges from "../VerificationBadges";
+import VerificationBadge from "../badges/VerificationBadge";
 import grayHeartIcon from '../../../public/images/heart_gray.svg'
 import redHeartIcon from '../../../public/images/heart_red.svg'
+import shareIcon from '../../../public/images/share.svg'
 
 interface IBadgeWrapper {
   width?: string
@@ -15,44 +16,47 @@ interface IProjectCardBadges {
   cardWidth?: string
   likes?: number
   verified?: boolean
+  isHover?: boolean
 }
 
 const ProjectCardBadges = (props: IProjectCardBadges) => {
-  const { cardWidth, likes, verified } = props
+  const { cardWidth, likes, verified, isHover } = props
   return (
     <BadgeWrapper width={cardWidth}>
       <div className='d-flex'>
-        {verified && <VerificationBadges verified /> }
-        {<VerificationBadges trace /> }
+        {verified && <VerificationBadge verified /> }
+        {<VerificationBadge trace /> }
       </div>
-      {heartBadge(true, likes)}
+      {heartBadge(true, likes, isHover)}
     </BadgeWrapper>
   )
 }
 
-const heartBadge = (active?: boolean, likes?: number) => {
+const heartBadge = (active?: boolean, likes?: number, isHover?: boolean) => {
   const HeartWrap = styled(FlexCenter)`
-    height: 30px;
+    height: ${() => isHover ? '72px' : '30px'};
     width: 30px;
-    border-radius: 50%;
-    background: ${() => {
-    if (active) return 'white'
-    else return Primary_Deep_800_Trans
-  }};
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    border-radius: 56px;
+    background: ${() => active ? 'white' : Primary_Deep_800_Trans};
   `
 
   const LikeBadge = styled(Subline_Bold)`
     color: white;
     margin-right: 6px;
+    margin-top: 7px;
   `
 
   return (
-    <FlexCenter>
+    <div className='d-flex'>
       {Number(likes) > 0 && <LikeBadge>{likes}</LikeBadge>}
       <HeartWrap>
         <Image src={active ? redHeartIcon : grayHeartIcon} alt='heart icon' />
+        {isHover && <Image src={shareIcon} alt='share icon'/>}
       </HeartWrap>
-    </FlexCenter>
+    </div>
   )
 }
 
