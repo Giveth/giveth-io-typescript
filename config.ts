83d3360763031f6ba4b092db0bdc04b672448c58
Ramin <r.ramazanpour@aut.ac.ir>
2021-11-15 02:38:31 +0330

    fix config for public images

--- config.ts
+++ config.ts
@@ -1,8 +1,12 @@
 const config = {
   QRAPHQL_SERVER: 'https://serve.giveth.io/graphql',
-  APP_URL: 'http://localhost:3000/',
+  APP_URL: 'https://typescript.giveth.io/',
   REPORT_ISSUE_URL: 'https://github.com/Giveth/giveth-io-typescript/issues/new',
   GIVeconomy_URL: 'https://liquidity-mining-dapp.vercel.app/'
+}
+
+if (process.env.NEXT_PUBLIC_ENVIRONMENT === 'develop') {
+  config.APP_URL = 'http://localhost:3000/'
 }
 
 export default config
