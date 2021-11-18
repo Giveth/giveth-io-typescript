fb73b1d25c616a1edcfe0d58e2aecaad7cdbe54c
Mateo Daza <mateodaza@gmail.com>
2021-11-17 22:16:51 -0500

    first design on join page

---
+++ pages/join.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../src/components/Footer'
+import Menubar from '../src/components/Menubar'
+import JoinIndex from '../src/components/views/join/JoinIndex'
+
+const Join = () => {
+  return (
+    <>
+      <Menubar />
+      <JoinIndex />
+      <Footer />
+    </>
+  )
+}
+
+export default Join
